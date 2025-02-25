91d9522b2a8db04150aa667cffed9f9ee331631b
Kunwoo Yoon <167579648+Kunbuns@users.noreply.github.com>
2025-02-25 16:39:07 -0500

    Completed 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,24 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+
+let newTemp = (tempF -32) * (5/9);
+
+if (typeof newTemp === 'number' && newTemp % 1 !== 0) { //Checks if number is float or integer
+  return +newTemp.toFixed(1); //Converts to string with 1 decimal using "toFixed(1)", then back to a number with "+"
+} else {
+  return newTemp;
+}
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+
+let newTemp = (tempC * (9/5) + 32);
+
+if (typeof newTemp === 'number' && newTemp % 1 !== 0) {
+  return +newTemp.toFixed(1);
+} else {
+  return newTemp;
+}
+
 };
 
 // Do not edit below this line
