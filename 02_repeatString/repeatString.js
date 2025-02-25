59c2b1cc10228211e436a3bc83934b640c4abaf3
Kunwoo Yoon <167579648+Kunbuns@users.noreply.github.com>
2025-02-25 15:06:21 -0500

    Completed 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
-};
+// I want repeatString to return the "string" inserted "num" number of times, all concentated with no spaces
+const repeatString = function(word, times) {
+  if (times < 0) return "ERROR";
+  let string = ""
+  for (let i = 0; i < times; i++) {
+    string += word;
+  }
+  return string;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
