334ef57e5e52a1f92687c2e59adf175c426b3e8b
Kunwoo Yoon <167579648+Kunbuns@users.noreply.github.com>
2025-02-25 16:00:49 -0500

    Completed 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,18 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+  if (typeof num1 !== 'number' || typeof num2 !== 'number' ||
+    num1 < 0 || num2 < 0 ||
+    num1 % 1 !== 0 || num2 % 1 !== 0) {
+    return "ERROR"
+  }
+
+  let sum = 0;
+  const start = Math.min(num1, num2);
+  const end = Math.max(num1, num2);
+
+  for (let i = start; i <= end; i++) {
+    sum += i;
+  }
+  return sum;
 
 };
 
