fe20e73178c9b914b02d237a9bc1cfe7abb046a7
Kunwoo Yoon <167579648+Kunbuns@users.noreply.github.com>
2025-03-13 17:37:47 -0400

    Completed 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    let array = [1, 1];
 
-};
+    if (typeof number === "string") {number = +number}; //string check
+    if (number === 0) {return 0}; //0 check
+    if (number < 0) {return "OOPS"}; //negative check
+
+
+    while (array.length < number) {
+        let nextValue = array[array.length - 1] + array[array.length - 2];
+        array.push(nextValue);
+    }
+
+    return array[number - 1];
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
